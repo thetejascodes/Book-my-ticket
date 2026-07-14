0c6b5a288e10e938af01e07215ef6169993ab02c
Tejas Kale <123163937+thetejascodes@users.noreply.github.com>
2026-07-14 13:02:27 +0000

    feat:- add registeDto for register user validation

---
+++ src/modules/auth/dto/register.dto.js
@@ -0,0 +1,13 @@
+import joi from 'joi'
+import BaseDto from '../../../common/dto/Base.Dto.js'
+
+class RegisterDto extends BaseDto {
+    static schema = joi.object({
+        name: joi.string().trim().min(3).max(30).required(),
+        email: joi.string().email().lowercase().trim().required(),
+        password: joi.string().min(8).message('Password must contain at least one uppercase letter and one digit').required(),
+        role: joi.string().valid('user', 'admin').default('user'),
+    })
+}
+
+export default RegisterDto;
