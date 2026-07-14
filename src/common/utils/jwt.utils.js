d5b8ac6148f6d94778e7561758c1a3dff8c4e78d
tejaskale99 <tejaskale1556@gmail.com>
2026-07-14 16:01:58 +0530

    feat:- add jwt tokens utils

--- src/common/utils/jwt.utils.js
+++ src/common/utils/jwt.utils.js
@@ -1,4 +1,17 @@
 import jwt from 'jsonwebtoken'
 export const generateToken = (payload) =>{
-    
+    return jwt.sign(payload,process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_ACCESS_EXPIRES_IN
+    })
+}
+export const verifyAccessToken = (token) =>{
+    return jwt.verify(token,process.env.JWT_SECRET)
+}
+export const generateRefreshToken = (payload) =>{
+    return jwt.sign(payload,process.env.JWT_REFRESH_SECRET,{
+        expiresIn:process.env.JWT_REFRESH_EXPIRES_IN
+    })
+}
+export const verifyRefreshToken = (token) =>{
+    return jwt.verify(token,process.env.JWT_REFRESH_SECRET)
 }
